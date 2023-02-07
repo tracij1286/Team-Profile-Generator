133730b31f7f3e3e444e83da8edf036d9f4ff426
tracij1286 <tracij50@gmail.com>
2023-02-07 18:53:53 -0500

    added test and html

---
+++ __test__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Manager1');
+
+    expect(typeof(manager)).toBe('object');
+});
+
+test('gets office number', () => {
+    const manager = new Manager('Manager', '1', 'tracij50@gmail.com','6');
+    expect(manager.getOfficeNumber()).toBe('6');
+});
+
+test('getRole() returns Manager', () => {
+    const manager = new Manager('Manager');
+
+    expect(manager.getRole()).toBe('Manager');
+}); 
